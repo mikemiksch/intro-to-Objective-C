be21ccdc2f494dd02d09085d0a472af1d3d73d5d
mikemiksch <mikemiksch@gmail.com>
2017-04-17 09:36:04 -0700

    Created Xcode project

---
+++ intro-to-Objective-C/intro-to-Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  intro-to-Objective-C
+//
+//  Created by Mike Miksch on 4/17/17.
+//  Copyright © 2017 Mike Miksch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
